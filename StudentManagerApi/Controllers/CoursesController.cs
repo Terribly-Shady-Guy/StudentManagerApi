2a5d88a94007f871b36b287ad52f7bdd15f52351
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-20 19:45:46 -0500

    added controller for courses

---
+++ StudentManagerApi/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagerApi.Models;
+
+namespace StudentManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> AddNewCourse(Course course)
+        {
+            return Ok(course);
+        }
+    }
+}
