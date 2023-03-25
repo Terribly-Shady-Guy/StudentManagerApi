484283b1454f39f91bec61f0b430b4630e40b9b2
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-25 17:15:21 -0500

    changed cors origin to match client url

--- StudentManagerApi/Program.cs
+++ StudentManagerApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000");
+        policy.WithOrigins("http://localhost:5173");
         policy.WithHeaders("content-type");
         policy.AllowCredentials();
         policy.WithMethods(new string[]
